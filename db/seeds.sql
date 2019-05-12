ab56c101595466e94c0fd60ff949a3d96436636b
CLDavis15 <calebldavis0@gmail.com>
2019-05-12 15:30:54 -0400

    set up git ignore and config file.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Smoker Burger", "YES")
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Carolina Burger", "NO")
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Breakfast Burger", "YES")
