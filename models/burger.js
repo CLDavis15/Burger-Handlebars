c4d3a5316e5e7958aea39a187e0249c4be972a25
CLDavis15 <calebldavis0@gmail.com>
2019-05-15 16:42:31 -0400

    final touch up in index.handlebars.

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(result) {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(result) {
             cb(result);
         });
     },
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(result) {
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(result) {
             cb(result);
         });
     },
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(result) {
-            cb(result);
-        });
-    },
-    deleteOne: function(condition, cb) {
-        orm.deleteOne("burgers", condition, function(res) {
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
             cb(res);
         });
     }
