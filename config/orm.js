7055bf8974d373d5c3de14272b9669361d98dcb3
CLDavis15 <calebldavis0@gmail.com>
2019-05-15 22:29:08 -0400

    fixed issues with handlebars.

--- config/orm.js
+++ config/orm.js
@@ -35,7 +35,7 @@ function printQuestionMarks(num) {
   
   // Object for all our SQL statement functions.
   var orm = {
-    selectAll: function(tableInput, cb) {
+    all: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
         if (err) {
@@ -44,7 +44,7 @@ function printQuestionMarks(num) {
         cb(result);
       });
     },
-    insertOne: function(table, cols, vals, cb) {
+    create: function(table, cols, vals, cb) {
       var queryString = "INSERT INTO " + table;
   
       queryString += " (";
@@ -64,7 +64,7 @@ function printQuestionMarks(num) {
         cb(result);
       });
     },
-    updateOne: function(table, objColVals, condition, cb) {
+    update: function(table, objColVals, condition, cb) {
       var queryString = "UPDATE " + table;
   
       queryString += " SET ";
@@ -81,7 +81,7 @@ function printQuestionMarks(num) {
         cb(result);
       });
     },
-     deleteOne: function(table, condition, cb) {
+     delete: function(table, condition, cb) {
       var queryString = "DELETE FROM " + table;
       queryString += " WHERE ";
       queryString += condition;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,13 +1,13 @@
 // Dependencies
 var express = require("express");
-// Import the model to use its db functions for burger.js
 var burger = require("../models/burger.js");
-
-// Create the router for the app, and export the router at the end of your file.
 var router = express.Router();
+
+
+
 // Create routes and set up logic where required.
 router.get("/", function (req, res) {
-    burger.selectAll(function(data) {
+    burger.all(function(data) {
         var hbsObject = {
             burgers: data
         };
@@ -15,38 +15,46 @@ router.get("/", function (req, res) {
         res.render("index", hbsObject);
     });
 });
-// Add new burger to the db.
+// POST route to create burger
 router.post("/api/burgers", function (req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.create(
+        ["burger_name", "devoured"], 
+    [req.body.name, req.body.devoured],
+     function(result) {
         // Send back the ID of the new burger
         res.json({ id: result.insertId });
     });
 });
-// Set burger devoured status to true.
+// PUT route to update burger status
 router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+    burger.update({ 
+        devoured: req.body.devoured 
+    }, condition, function(result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
             return res.status(404).end();
-        } else {
+        } 
+        else {
             res.status(200).end();
         }
     });
 });
-// Delete burger from db.
+// Delete burger route
 router.delete("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
+
     console.log("condition", condition);
 
-    burger.deleteOne(condition, function(result) {
+    burger.delete(condition, function(result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
             return res.status(404).end();
-        } else {
+        } 
+        else {
             res.status(200).end();
         }
     });
