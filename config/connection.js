779a4d92db678ce5db768c1116293c12f778a8f9
CLDavis15 <calebldavis0@gmail.com>
2019-05-20 16:08:20 -0400

    fixed Jawsdb.

--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,20 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 8889,
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 8889,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    })
+};
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-
-  var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
-}
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+var mysql = require('mysql');
 // Export connection for our ORM to use.
 module.exports = connection;
